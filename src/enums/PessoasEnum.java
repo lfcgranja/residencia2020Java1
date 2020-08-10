2064281df88cda9bceea67b60ac7dbc4f1947b45
Marcelo Collares <marcelo.collares@neki-it.com.br>
2020-08-10 14:02:10 -0300

    Incluindo enuns e teste enuns

---
+++ src/enums/PessoasEnum.java
@@ -0,0 +1,10 @@
+package enums;
+
+public enum PessoasEnum {
+
+	GERENTE, 
+	DIRETOR, 
+	PRESIDENTE, 
+	CLIENTE;
+	
+}
---
+++ src/residencia/PrincipalTestaEnums.java
@@ -0,0 +1,24 @@
+package residencia;
+
+import enums.PessoasEnum;
+
+public class PrincipalTestaEnums {
+	
+	public static void main(String[] args) {
+
+	String testaEnum = "PRESIDENTE";
+	
+	//Posso atribuir a uma variável PessoasEnum
+	PessoasEnum presidente = PessoasEnum.PRESIDENTE;
+	PessoasEnum gerente = PessoasEnum.GERENTE;
+	
+	//Posso usar diretamente a uma comparação dentro de um if
+	if( testaEnum.equalsIgnoreCase("PRESIDENTE")) {
+		System.out.println("Tipo da Pessoa = " + presidente.name());
+	}
+	else if( testaEnum.equalsIgnoreCase("GERENTE")) {
+		System.out.println("Tipo da Pessoa = " + gerente.name());
+	}
+	
+	}
+}	
